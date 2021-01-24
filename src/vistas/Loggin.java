5f30f7c56cca8e790817aa8e4da109a4a1986b5d
FF <5amarinaflo@gmail.com>
2021-01-24 12:16:11 +0100

    quitado el olvidar contraseña para implementarlo más adelante

--- src/vistas/Loggin.java
+++ src/vistas/Loggin.java
@@ -49,98 +49,14 @@ public class Loggin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDialogOlvidarPwd = new javax.swing.JDialog();
-        jPanelOlvidar = new javax.swing.JPanel();
-        jLabelTituloOlvidarPwd = new javax.swing.JLabel();
-        jTextArea1 = new javax.swing.JTextArea();
-        jTextFOlvidarCorreo = new javax.swing.JTextField();
-        jButtonOlvidar = new javax.swing.JButton();
         jPanelInicioSesion = new javax.swing.JPanel();
         TextFUsuario = new javax.swing.JTextField();
         TextPassPwd = new javax.swing.JPasswordField();
         jLabelUser = new javax.swing.JLabel();
         jLabelPwd = new javax.swing.JLabel();
-        jLabelOlvidarPwd = new javax.swing.JLabel();
         jButtonIncioSesion = new javax.swing.JButton();
         jLabelRegistrarse = new javax.swing.JLabel();
         jLabelFotoLogo = new javax.swing.JLabel();
-
-        jDialogOlvidarPwd.setTitle("Olvidar contraseña");
-        jDialogOlvidarPwd.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-
-        jPanelOlvidar.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
-
-        jLabelTituloOlvidarPwd.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
-        jLabelTituloOlvidarPwd.setText("¿Te has olvidado la contraseña?");
-
-        jTextArea1.setEditable(false);
-        jTextArea1.setBackground(new java.awt.Color(212, 210, 210));
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(5);
-        jTextArea1.setText("Introduce tu correo electronico para poder enviarte\n un correo con la nueva contraseña. \n\nEs recomendame que luego la cambies por otra.");
-        jTextArea1.setWrapStyleWord(true);
-        jTextArea1.setAutoscrolls(false);
-        jTextArea1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
-        jTextFOlvidarCorreo.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
-
-        jButtonOlvidar.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
-        jButtonOlvidar.setText("Cambiar contraseña");
-        jButtonOlvidar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonOlvidarActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout jPanelOlvidarLayout = new javax.swing.GroupLayout(jPanelOlvidar);
-        jPanelOlvidar.setLayout(jPanelOlvidarLayout);
-        jPanelOlvidarLayout.setHorizontalGroup(
-            jPanelOlvidarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelOlvidarLayout.createSequentialGroup()
-                .addGroup(jPanelOlvidarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanelOlvidarLayout.createSequentialGroup()
-                        .addGap(42, 42, 42)
-                        .addComponent(jLabelTituloOlvidarPwd))
-                    .addGroup(jPanelOlvidarLayout.createSequentialGroup()
-                        .addGap(140, 140, 140)
-                        .addComponent(jTextFOlvidarCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanelOlvidarLayout.createSequentialGroup()
-                        .addGap(150, 150, 150)
-                        .addComponent(jButtonOlvidar)))
-                .addContainerGap(52, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelOlvidarLayout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(63, 63, 63))
-        );
-        jPanelOlvidarLayout.setVerticalGroup(
-            jPanelOlvidarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanelOlvidarLayout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(jLabelTituloOlvidarPwd)
-                .addGap(18, 18, 18)
-                .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(16, 16, 16)
-                .addComponent(jTextFOlvidarCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButtonOlvidar)
-                .addContainerGap(32, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout jDialogOlvidarPwdLayout = new javax.swing.GroupLayout(jDialogOlvidarPwd.getContentPane());
-        jDialogOlvidarPwd.getContentPane().setLayout(jDialogOlvidarPwdLayout);
-        jDialogOlvidarPwdLayout.setHorizontalGroup(
-            jDialogOlvidarPwdLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDialogOlvidarPwdLayout.createSequentialGroup()
-                .addComponent(jPanelOlvidar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        jDialogOlvidarPwdLayout.setVerticalGroup(
-            jDialogOlvidarPwdLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanelOlvidar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio de sesión");
@@ -163,13 +79,6 @@ public class Loggin extends javax.swing.JFrame {
 
         jLabelPwd.setText("Contraseña:");
 
-        jLabelOlvidarPwd.setText("Olvidar contraseña...");
-        jLabelOlvidarPwd.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jLabelOlvidarPwdMouseClicked(evt);
-            }
-        });
-
         jButtonIncioSesion.setText("Iniciar Sesión");
         jButtonIncioSesion.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -185,7 +94,6 @@ public class Loggin extends javax.swing.JFrame {
         });
 
         jLabelFotoLogo.setBackground(new java.awt.Color(51, 51, 255));
-        jLabelFotoLogo.setText("jLabel1");
 
         javax.swing.GroupLayout jPanelInicioSesionLayout = new javax.swing.GroupLayout(jPanelInicioSesion);
         jPanelInicioSesion.setLayout(jPanelInicioSesionLayout);
@@ -207,11 +115,9 @@ public class Loggin extends javax.swing.JFrame {
                             .addComponent(jLabelPwd)
                             .addComponent(jLabelRegistrarse))
                         .addGap(66, 66, 66)
-                        .addGroup(jPanelInicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabelOlvidarPwd)
-                            .addGroup(jPanelInicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(TextPassPwd, javax.swing.GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
-                                .addComponent(TextFUsuario)))
+                        .addGroup(jPanelInicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(TextPassPwd, javax.swing.GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
+                            .addComponent(TextFUsuario))
                         .addGap(81, 81, 81))))
         );
         jPanelInicioSesionLayout.setVerticalGroup(
@@ -228,9 +134,7 @@ public class Loggin extends javax.swing.JFrame {
                     .addComponent(TextPassPwd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabelPwd))
                 .addGap(19, 19, 19)
-                .addGroup(jPanelInicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelOlvidarPwd)
-                    .addComponent(jLabelRegistrarse))
+                .addComponent(jLabelRegistrarse)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButtonIncioSesion)
                 .addContainerGap(39, Short.MAX_VALUE))
@@ -262,22 +166,6 @@ public class Loggin extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Error algún campo está vacío", "Error de campo vacío", JOptionPane.CLOSED_OPTION);
         }
     }//GEN-LAST:event_jButtonIncioSesionActionPerformed
-
-    private void jLabelOlvidarPwdMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelOlvidarPwdMouseClicked
-        //volvemos invisible esta venta
-        // dispose();
-
-        //Redirigmos a 
-        jDialogOlvidarPwd.setModal(true);
-        jDialogOlvidarPwd.setLocationRelativeTo(null);
-        jDialogOlvidarPwd.setSize(500, 300);
-        jDialogOlvidarPwd.setVisible(true);
-    }//GEN-LAST:event_jLabelOlvidarPwdMouseClicked
-
-    private void jButtonOlvidarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonOlvidarActionPerformed
-        String destinatario = this.jTextFOlvidarCorreo.getText();
-        this.enviarCorreo(destinatario);
-    }//GEN-LAST:event_jButtonOlvidarActionPerformed
 
     private void jLabelRegistrarseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelRegistrarseMouseClicked
         JDialogRegistrarse regis = new JDialogRegistrarse(this, true);
@@ -381,83 +269,16 @@ public class Loggin extends javax.swing.JFrame {
         }
     }
 
-    private void enviarCorreo(String destinatario) {
-        Usuarios user = null;
-
-//        user = conectBD.existeUsu(destinatario);
-//        if (user != null) {
-        // Esto es lo que va delante de @gmail.com en tu cuenta de correo. Es el remitente también.
-        final String remitente = "tiendapelis2021@gmail.com";  //Para la dirección nomcuenta@gmail.com
-        final String password = "aplicacionPelis20";
-
-        //Properties => parámetros de configuración para el servidor de correo para crear la sesión
-        Properties props = System.getProperties(); //new Properties();
-        props.put("mail.smtp.user", remitente);
-        props.put("mail.smtp.clave", password);    //La clave de la cuenta
-        props.put("mail.smtp.auth", "true");//Usar autenticación mediante usuario y clave
-        props.put("mail.smtp.starttls.enable", "true");//Para conectar de manera segura al servidor SMTP, TLS si está disponible
-        props.put("mail.smtp.host", "smtp.gmail.com");//El servidor SMTP de Google
-        props.put("mail.smtp.port", "587");//El puerto SMTP seguro de Google
-
-        //Session => representa la conexión con el servidor gmail de correo
-        Session session = Session.getDefaultInstance(props);
-        session.setDebug(true); //para obtener más información por pantalla de lo que está sucediendo
-
-        // MimeMessage => es el mensaje
-        MimeMessage message = new MimeMessage(session);
-        String asunto = "Cambiar contraseña de ";
-        String nuevaPwd = generarContraseña();
-
-        this.conectBD.openBD();
-        //conectBD.updatePwd(destinatario, nuevaPwd);
-        this.conectBD.closeBD();
-
-        System.out.println("Nueva contraseña " + nuevaPwd);
-        String cuerpo = "Buenas. \n"
-                + "Ha pulsado en que se le ha olvidado la contraseña. \n"
-                + "Aquí tiene la nueva contraseña " + nuevaPwd
-                + "Saludos. "
-                + "Para cualquier problema contactenos";
-
-        try {
-            message.setFrom(new InternetAddress(remitente)); //quien lo manda
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario)); //Quien lo recibe
-            message.setSubject(asunto); //el asunto del mensaje
-            message.setText(cuerpo); //el cuerpo del mensaje
-
-            // Transport => Clase para el envío de mensajes, enlazamos el Transport con la Session
-            Transport transport = session.getTransport("smtp"); //nombre del protocolo a usar.
-            transport.connect("smtp.gmail.com", remitente, password); //establecemos la conexión, dando el nombre de usuario y password.
-            transport.sendMessage(message, message.getAllRecipients()); //enviamos el mensaje
-            transport.close(); //cerramos la conexión
-
-        } catch (MessagingException me) {
-            me.printStackTrace();   //Si se produce un error
-        }
-//        }else{
-//            JOptionPane.showMessageDialog(null, "Error en el correo", "Error", JOptionPane.CLOSED_OPTION);
-//        }
-    }
-
-    private String generarContraseña() {
-        return RandomStringUtils.random(8, 0, 0, true, true, null, new SecureRandom());
-    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField TextFUsuario;
     private javax.swing.JPasswordField TextPassPwd;
     private javax.swing.JButton jButtonIncioSesion;
-    private javax.swing.JButton jButtonOlvidar;
-    private javax.swing.JDialog jDialogOlvidarPwd;
     private javax.swing.JLabel jLabelFotoLogo;
-    private javax.swing.JLabel jLabelOlvidarPwd;
     private javax.swing.JLabel jLabelPwd;
     private javax.swing.JLabel jLabelRegistrarse;
-    private javax.swing.JLabel jLabelTituloOlvidarPwd;
     private javax.swing.JLabel jLabelUser;
     private javax.swing.JPanel jPanelInicioSesion;
-    private javax.swing.JPanel jPanelOlvidar;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextFOlvidarCorreo;
     // End of variables declaration//GEN-END:variables
 
 }
